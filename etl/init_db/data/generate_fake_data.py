648e74e7f32cf569827c1ce48ed86cc3e9957eb1
Mosyagin Grigorii <mosyagin.grisha@gmail.com>
2024-06-05 22:54:01 +0300

    refactor: Изменены данные

--- etl/init_db/data/generate_fake_data.py
+++ etl/init_db/data/generate_fake_data.py
@@ -13,15 +13,13 @@ FILE_EXTENSIONS = [
     ".wav",
     ".mp3"
 ]
-FILE_EXT_NUMBER = [1, 2, 3, 4]
 STATUS = [1, 0]
 
 SHORT_NAME_TABLE_DATA = []
 FULL_NAME_TABLE_DATA = []
 
 for short_filename in short_filenames:
-    number = random.sample(FILE_EXT_NUMBER, k=1)
-    extentions = random.sample(FILE_EXTENSIONS, k=number[0])
+    extentions = random.sample(FILE_EXTENSIONS, k=1)
     status = random.sample(STATUS, k=1)
     SHORT_NAME_TABLE_DATA.append(
         {
@@ -29,15 +27,14 @@ for short_filename in short_filenames:
             "status": status[0]
         }
     )
-    for this_extention in extentions:
-        FULL_NAME_TABLE_DATA.append(
-            {
-                "name": short_filename + this_extention,
-                "status": None
-            }
-        )
+    FULL_NAME_TABLE_DATA.append(
+        {
+            "name": short_filename + extentions[0],
+            "status": None
+        }
+    )
 
 FULL_NAME_TABLE_DATA = random.sample(FULL_NAME_TABLE_DATA, k=500_000)
 
-pd.DataFrame(SHORT_NAME_TABLE_DATA).to_csv("full_data.csv")
-pd.DataFrame(FULL_NAME_TABLE_DATA).to_csv("short_data.csv")
+pd.DataFrame(SHORT_NAME_TABLE_DATA).to_csv("short_data.csv")
+pd.DataFrame(FULL_NAME_TABLE_DATA).to_csv("full_data.csv")
